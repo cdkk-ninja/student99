e48ca1b1b9b1e072d07f9286a00a6e07f1500602
BrianDunneKK <Brian.Dunne.KK@gmail.com>
2022-02-20 21:35:28 +0000

    Add comments

--- Say Hello.py
+++ Say Hello.py
@@ -1,3 +1,5 @@
-name = input("What is your name? ")
-print("Hello "+name+"!")
+# Change the question to: What is your name?
+# Add an exclamation mark after saying hello ... for example: Hello Jo!
+name = input("Who are you? ")
+print("Hello " + name)
 print("Welcome to the world of Python.")
